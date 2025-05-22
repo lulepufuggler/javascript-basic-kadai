0184b08776e4582c212016fc8488800ecae94bbd
lulepufuggler <misarinmii@gmail.com>
2025-05-22 20:57:34 +0900

    kadai_007をアップロード1

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -6,11 +6,11 @@ if (num % 3 == 0 && num % 5 == 0 ) {
   console.log('3と5の倍数です');
 }
 
-if (num % 3 == 0) {
+else if (num % 3 == 0) {
   console.log('3の倍数です');
 }
 
-if (num % 5 == 0) {
+else if (num % 5 == 0) {
   console.log('5の倍数です');
 }
 
